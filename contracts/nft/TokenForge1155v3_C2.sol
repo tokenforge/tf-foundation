59d0c01a34a972fa4281a8166dc3a95ea2cccead
Hagen Hübel <hhuebel@itinance.com>
2022-09-30 22:06:55 +0200

    nft contracts and deployments

---
+++ contracts/nft/TokenForge1155v3_C2.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+// (C) by TokenForge GmbH, Berlin
+// Author: Hagen Hübel, hagen@token-forge.io
+
+pragma solidity >=0.8.3;
+
+import "./TokenForge1155v3.sol";
+
+contract TokenForge1155v3_C2 is TokenForge1155v3 {
+    constructor(address signer_, string memory baseUri_) TokenForge1155v3(signer_, baseUri_) {
+    }
+
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(TokenForge1155v3)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
