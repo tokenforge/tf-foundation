21d4541fdd568b6ca90739b125888ae3f9f1a702
Hagen Hübel <hhuebel@itinance.com>
2022-09-30 23:49:08 +0200

    linting

--- contracts/nft/TokenForge1155v3_Demo.sol
+++ contracts/nft/TokenForge1155v3_Demo.sol
@@ -7,17 +7,9 @@ pragma solidity >=0.8.3;
 import "./TokenForge1155v3.sol";
 
 contract TokenForge1155v3_Demo is TokenForge1155v3 {
-    constructor(address signer_, string memory baseUri_) TokenForge1155v3(signer_, baseUri_) {
-    }
+    constructor(address signer_, string memory baseUri_) TokenForge1155v3(signer_, baseUri_) {}
 
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override(TokenForge1155v3)
-        returns (bool)
-    {
+    function supportsInterface(bytes4 interfaceId) public view virtual override(TokenForge1155v3) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
