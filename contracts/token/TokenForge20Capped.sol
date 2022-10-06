073ee63038db52bd0ebb24d996270a4328801775
Hagen Hübel <hhuebel@itinance.com>
2022-10-06 22:41:56 +0200

    linting

--- contracts/token/TokenForge20Capped.sol
+++ contracts/token/TokenForge20Capped.sol
@@ -5,7 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenForge20Capped is ERC20Capped, Ownable {
-    constructor(string memory name, string memory symbol, uint256 cap) ERC20(name, symbol) ERC20Capped(cap) {}
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 cap
+    ) ERC20(name, symbol) ERC20Capped(cap) {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
